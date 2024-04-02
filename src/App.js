0b6a633964ee98a310a12d6ce99e54971a12c286
karolinaobarzanek <karolina.gasior2308@gmail.com>
2024-04-02 17:06:50 +0200

    Creating async function fetchMovies()

--- src/App.js
+++ src/App.js
@@ -55,11 +55,15 @@ const average = (arr) =>
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
+  const query = "interstellar";
 
 useEffect(function() {
-    fetch(`http://www.omdbapi.com/?s=interstellar&apikey=${KEY}`)
-    .then((res) => res.json())
-    .then((data) => setMovies(data.Search));
+  async function fetchMovies() {
+    const res = await fetch(`http://www.omdbapi.com/?s=${query}&apikey=${KEY}`)
+    const data = await res.json()
+    setMovies(data.Search)
+    }
+    fetchMovies();
   }, []);
 
   return (
