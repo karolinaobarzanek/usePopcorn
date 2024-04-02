d95d8f983daac94d16cef58061c0b4bd0dde0475
karolinaobarzanek <karolina.gasior2308@gmail.com>
2024-04-02 16:30:51 +0200

    Fixing bug in index.css file

--- src/App.js
+++ src/App.js
@@ -68,43 +68,45 @@ useEffect(function() {
         <Search />
         <NumResults movies={movies} />
       </NavBar>
+
       <Main>
         <Box>
           <MovieList movies={movies} />
         </Box>
+
         <Box>
           <WatchedSummary watched={watched} />
           <WatchedMoviesList watched={watched} />
         </Box>
       </Main>
     </>
+  );
+}
+
+  function NavBar({ children }) {
+    return (
+      <nav className="nav-bar">
+        <Logo />
+        {children}
+      </nav>
     );
   }
 
-function NavBar({children}) {
-  
+function NumResults({ movies }) {
   return (
-    <nav className="nav-bar">
-      <Logo />
-      {children}
-    </nav>
+    <p className="num-results">
+      Found <strong>{movies.length}</strong> results
+    </p>
   );
 }
 
-function NumResults({movies}) {
-  return (
-    <p className="num-results">
-        Found <strong>{movies.length}</strong> results
-      </p>
-  );
-}
 
 function Logo() {
   return (
     <div className="logo">
-        <span role="img">🍿</span>
-        <h1>usePopcorn</h1>
-      </div>
+      <span role="img">🍿</span>
+      <h1>usePopcorn</h1>
+    </div>
   );
 }
 
@@ -113,30 +115,28 @@ function Search() {
 
   return (
     <input
-        className="search"
-        type="text"
-        placeholder="Search movies..."
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-      />
+      className="search"
+      type="text"
+      placeholder="Search movies..."
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+    />
   );
-} 
+}
 
-function Main({children}) {
-  
+function Main({ children }) {
   return <main className="main">{children}</main>;
 }
 
-function Box({children}) {
+function Box({ children }) {
   const [isOpen, setIsOpen] = useState(true);
 
   return (
     <div className="box">
-      <button
-        className="btn-toggle"
-        onClick={() => setIsOpen((open) => !open)}>
+      <button className="btn-toggle" onClick={() => setIsOpen((open) => !open)}>
         {isOpen ? "–" : "+"}
       </button>
+
       {isOpen && children}
     </div>
   );
@@ -167,8 +167,7 @@ function WatchedBox() {
 }  
 */
 
-function MovieList({movies}) {
-
+function MovieList({ movies }) {
   return (
     <ul className="list">
       {movies?.map((movie) => (
@@ -178,31 +177,29 @@ function MovieList({movies}) {
   );
 }
 
-function Movie({movie}) {
+
+function Movie({ movie }) {
   return (
     <li>
       <img src={movie.Poster} alt={`${movie.Title} poster`} />
-        <h3>{movie.Title}</h3>
-        <div>
-          <p>
-            <span>🗓</span>
-            <span>{movie.Year}</span>
-          </p>
-        </div>
+      <h3>{movie.Title}</h3>
+      <div>
+        <p>
+          <span>🗓</span>
+          <span>{movie.Year}</span>
+        </p>
+      </div>
     </li>
   );
 }
 
 
-
-function WatchedSummary({watched}) {
+function WatchedSummary({ watched }) {
   const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
   const avgUserRating = average(watched.map((movie) => movie.userRating));
   const avgRuntime = average(watched.map((movie) => movie.runtime));
 
-
   return (
-   
     <div className="summary">
       <h2>Movies you watched</h2>
       <div>
@@ -221,23 +218,23 @@ function WatchedSummary({watched}) {
         <p>
           <span>⏳</span>
           <span>{avgRuntime} min</span>
-        </p>    
+        </p>
       </div>
     </div>
   );
 }
 
-function WatchedMoviesList({watched}) {
+function WatchedMoviesList({ watched }) {
   return (
     <ul className="list">
       {watched.map((movie) => (
-      <WatchedMovie movie={movie} key={movie.imdbID}/>
+        <WatchedMovie movie={movie} key={movie.imdbID} />
       ))}
     </ul>
   );
 }
 
-function WatchedMovie({movie}) {
+function WatchedMovie({ movie }) {
   return (
     <li>
       <img src={movie.Poster} alt={`${movie.Title} poster`} />
