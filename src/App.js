985467ff4cd701411e46a9e46facee26d906c1c4
karolinaobarzanek <karolina.gasior2308@gmail.com>
2024-04-04 12:54:10 +0200

    Exporting Box, Main, ErrorMessage, Loader, Movie, MovieList components into separate files

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import StarRaring from './StarRating';
 import NavBar from "./NavBar";
 import NumResults from "./NumResults";
 import Search from "./Search";
+import Main from "./Main";
+import Box from "./Box";
+import Loader from "./Loader";
+import MovieList from "./MovieList";
+import ErrorMessage from "./ErrorMessage";
 
 
 const average = (arr) =>
@@ -75,7 +80,6 @@ export default function App() {
 
       <Main>
         <Box>
-          {/*{isLoading ? <Loader /> : <MovieList movies={movies} />}*/}
           {isLoading && <Loader />}
           {!isLoading && !error && <MovieList movies={movies} onSelectMovie={handleSelectMovie} />}
           {error && <ErrorMessage message={error} />}
@@ -98,126 +102,6 @@ export default function App() {
         </Box> 
       </Main>
     </>
-  );
-}
-
-function Loader() {
-  return <p className="loader">Loading...</p>
-}
-
-function ErrorMessage({message}) {
-  return (
-    <p className="error">
-    <span>⛔️</span>{message}
-    </p>
-  );
-}
- 
-{/*function NavBar({ children }) {
-  return (
-    <nav className="nav-bar">
-      <Logo />
-      {children}
-    </nav>
-  );
-  }*/}
-
-{/*function NumResults({ movies }) {
-  return (
-    <p className="num-results">
-      Found <strong>{movies.length}</strong> results
-    </p>
-  );
-}*/}
-
-
-{/*function Logo() {
-  return (
-    <div className="logo">
-      <span role="img">🍿</span>
-      <h1>usePopcorn</h1>
-    </div>
-  );
-}*/}
-
-{/*function Search({query, setQuery}) {
-  return (
-    <input
-      className="search"
-      type="text"
-      placeholder="Search movies..."
-      value={query}
-      onChange={(e) => setQuery(e.target.value)}
-    />
-  );
-}*/}
-
-function Main({ children }) {
-  return <main className="main">{children}</main>;
-}
-
-function Box({ children }) {
-  const [isOpen, setIsOpen] = useState(true);
-
-  return (
-    <div className="box">
-      <button className="btn-toggle" onClick={() => setIsOpen((open) => !open)}>
-        {isOpen ? "–" : "+"}
-      </button>
-
-      {isOpen && children}
-    </div>
-  );
-}
-
- /*
-function WatchedBox() {
-  const [watched, setWatched] = useState(tempWatchedData);
-  const [isOpen2, setIsOpen2] = useState(true);
-
-  
-  return (
-    <div className="box">
-      <button
-        className="btn-toggle"
-        onClick={() => setIsOpen2((open) => !open)}
-      >
-        {isOpen2 ? "–" : "+"}
-      </button>
-      {isOpen2 && (
-        <>
-          <WatchedSummary watched={watched} />
-          <WatchedMoviesList watched={watched} />
-        </>
-      )}
-    </div>
-  );
-}  
-*/
-
-function MovieList({ movies, onSelectMovie}) {
-  return (
-    <ul className="list list-movies">
-      {movies?.map((movie) => (
-        <Movie movie={movie} key={movie.imdbID} onSelectMovie={onSelectMovie} />
-      ))}
-    </ul>
-  );
-}
-
-
-function Movie({ movie, onSelectMovie }) {
-  return (
-    <li onClick={() => onSelectMovie(movie.imdbID)}>
-      <img src={movie.Poster} alt={`${movie.Title} poster`} />
-      <h3>{movie.Title}</h3>
-      <div>
-        <p>
-          <span>🗓</span>
-          <span>{movie.Year}</span>
-        </p>
-      </div>
-    </li>
   );
 }
 
