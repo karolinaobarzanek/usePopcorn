7ab9d1402d56ff918ef5bffb4bc994c0b7df5de3
karolinaobarzanek <karolina.gasior2308@gmail.com>
2024-04-10 12:59:30 +0200

    Activating searchbar by pressing Enter key

--- src/components/navbar-components/Search.js
+++ src/components/navbar-components/Search.js
@@ -4,8 +4,19 @@ export default function Search({query, setQuery}) {
   const inputEl = useRef(null);
 
   useEffect(function() {
-    inputEl.current.focus();
-  },[]);
+    function callback(e) {
+
+      if(document.activeElement === inputEl.current) return;
+
+      if(e.code === "Enter") {
+        inputEl.current.focus();
+        setQuery("");
+      }
+    }
+    document.addEventListener('keydown', callback);
+    return () =>document.addEventListener('keydown', callback);
+    
+  },[setQuery]);
 
     return (
           <input
